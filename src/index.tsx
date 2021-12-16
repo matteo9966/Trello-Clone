64ed101de0e031e4cf286a3c39729e855becf98b
matteo9966 <matteoluigitommasi@gmail.com>
2021-12-16 14:51:18 +0100

    risolto errore redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { GlobalThemeProvider } from "./GlobalTheme/ThemeProvider";
-
+import { Provider } from "react-redux";
+import store from "./state/store";
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={store}>
     <GlobalThemeProvider>
       <App />
+      
     </GlobalThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
